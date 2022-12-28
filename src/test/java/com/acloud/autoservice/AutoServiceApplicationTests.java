ce854bd485e919648da3dd73d40486109748f15f
Dongzhu <tony@idongzhu.com>
2022-12-28 12:36:57 +0800

    Built the project and modules structure and directories. built passed. 初步构建项目和模块结构和目录结构，项目整体build通过。

---
+++ src/test/java/com/acloud/autoservice/AutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acloud.autoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
